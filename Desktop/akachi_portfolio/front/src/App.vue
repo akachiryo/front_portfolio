38c417e0e7cdcf9fc739e3ffd120692e226e2ddf
akachiryo <rirugameri@icloud.com>
2022-06-01 20:51:28 +0900

    firebase deploy

--- Desktop/akachi_portfolio/front/src/App.vue
+++ Desktop/akachi_portfolio/front/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+    <h2>akachi</h2>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProfileTop />
   </div>
 </template>
 
