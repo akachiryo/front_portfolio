357a58faa1edecdf759a25176c724cfd5900e92c
akachiryo <rirugameri@icloud.com>
2022-06-02 22:27:07 +0900

    header

--- Desktop/akachi_portfolio/front/src/App.vue
+++ Desktop/akachi_portfolio/front/src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
+    <AppHeader />
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
+    <AppFooter />
   </div>
 </template>
 
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ Desktop/akachi_portfolio/front/src/components/AppHeader.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="header">
+    <p class="header-title">My Portfolio</p>
+    <button class="login-button">
+      <span class="login-button-text">ログイン</span>
+      <!-- ログイン -->
+    </button>
+  </div>
+</template>
+
+<style>
+.header {
+  /* width: 1440px;
+  height: 120px;
+  padding: 0, 40;
+  display: flex;
+  justify-content: space-between;
+  /* background-color: #1b5678; */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 40px;
+  gap: 10px;
+  position: absolute;
+  width: 1440px;
+  height: 120px;
+  left: 0px;
+  top: 0px;
+  background-color: #1b5678;
+}
+
+.header-title {
+  width: 201px;
+  height: 42px;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 42px;
+  color: #ffffff;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  flex-shrink: 0;
+  white-space: nowrap;
+}
+
+.login-button {
+  /* width: 152px;
+  height: 53px;
+  margin-top: 33.5px;
+  margin-right: 40px;
+  padding: 16, 40;
+  border-radius: 4px;
+  flex-shrink: 0; */
+  /* Auto layout */
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 40px;
+  width: 152px;
+  height: 53px;
+  background: #ffffff;
+  border-radius: 4px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+}
+
+.login-button-text {
+  font-family: Roboto, sans-serif;
+  width: 72px;
+  height: 21px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+  color: rgba(0, 0, 0, 0.75);
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+}
+</style>
